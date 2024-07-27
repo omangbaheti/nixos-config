87c79c455e78ac479a38be30310b3e6576fffc78
Baheti <60500898+omangbaheti@users.noreply.github.com>
2024-07-27 16:55:12 -0700

    Reorganising

--- flake.nix
+++ flake.nix
@@ -1,203 +1,5 @@
 {
-  description = "Flake of LibrePhoenix";
-
-  outputs = inputs@{ self, ... }:
-    let
-      # ---- SYSTEM SETTINGS ---- #
-      systemSettings = {
-        system = "x86_64-linux"; # system arch
-        hostname = "snowfire"; # hostname
-        profile = "personal"; # select a profile defined from my profiles directory
-        timezone = "America/Chicago"; # select timezone
-        locale = "en_US.UTF-8"; # select locale
-        bootMode = "uefi"; # uefi or bios
-        bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
-        grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
-        gpuType = "amd"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
-      };
-
-      # ----- USER SETTINGS ----- #
-      userSettings = rec {
-        username = "emmet"; # username
-        name = "Emmet"; # name/identifier
-        email = "emmet@librephoenix.com"; # email (used for certain configurations)
-        dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
-        theme = "io"; # selcted theme from my themes directory (./themes/)
-        wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
-        # window manager type (hyprland or x11) translator
-        wmType = if (wm == "hyprland") then "wayland" else "x11";
-        browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
-        defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
-        term = "alacritty"; # Default terminal command;
-        font = "Intel One Mono"; # Selected font
-        fontPkg = pkgs.intel-one-mono; # Font package
-        editor = "emacsclient"; # Default editor;
-        # editor spawning translator
-        # generates a command that can be used to spawn editor inside a gui
-        # EDITOR and TERM session variables must be set in home.nix or other module
-        # I set the session variable SPAWNEDITOR to this in my home.nix for convenience
-        spawnEditor = if (editor == "emacsclient") then
-                        "emacsclient -c -a 'emacs'"
-                      else
-                        (if ((editor == "vim") ||
-                             (editor == "nvim") ||
-                             (editor == "nano")) then
-                               "exec " + term + " -e " + editor
-                         else
-                           editor);
-      };
-
-      # create patched nixpkgs
-      nixpkgs-patched =
-        (import inputs.nixpkgs { system = systemSettings.system; rocmSupport = (if systemSettings.gpu == "amd" then true else false); }).applyPatches {
-          name = "nixpkgs-patched";
-          src = inputs.nixpkgs;
-          patches = [ ./patches/emacs-no-version-check.patch ];
-        };
-
-      # configure pkgs
-      # use nixpkgs if running a server (homelab or worklab profile)
-      # otherwise use patched nixos-unstable nixpkgs
-      pkgs = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
-              then
-                pkgs-stable
-              else
-                (import nixpkgs-patched {
-                  system = systemSettings.system;
-                  config = {
-                    allowUnfree = true;
-                    allowUnfreePredicate = (_: true);
-                  };
-                  overlays = [ inputs.rust-overlay.overlays.default ];
-                }));
-
-      pkgs-stable = import inputs.nixpkgs-stable {
-        system = systemSettings.system;
-        config = {
-          allowUnfree = true;
-          allowUnfreePredicate = (_: true);
-        };
-      };
-
-      pkgs-unstable = import inputs.nixpkgs-patched {
-        system = systemSettings.system;
-        config = {
-          allowUnfree = true;
-          allowUnfreePredicate = (_: true);
-        };
-        overlays = [ inputs.rust-overlay.overlays.default ];
-      };
-
-      pkgs-emacs = import inputs.emacs-pin-nixpkgs {
-        system = systemSettings.system;
-      };
-
-      pkgs-kdenlive = import inputs.kdenlive-pin-nixpkgs {
-        system = systemSettings.system;
-      };
-
-      pkgs-nwg-dock-hyprland = import inputs.nwg-dock-hyprland-pin-nixpkgs {
-        system = systemSettings.system;
-      };
-
-      # configure lib
-      # use nixpkgs if running a server (homelab or worklab profile)
-      # otherwise use patched nixos-unstable nixpkgs
-      lib = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
-             then
-               inputs.nixpkgs-stable.lib
-             else
-               inputs.nixpkgs.lib);
-
-      # use home-manager-stable if running a server (homelab or worklab profile)
-      # otherwise use home-manager-unstable
-      home-manager = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
-             then
-               inputs.home-manager-stable
-             else
-               inputs.home-manager-unstable);
-
-      # Systems that can run tests:
-      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-
-      # Function to generate a set based on supported systems:
-      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-
-      # Attribute set of nixpkgs for each system:
-      nixpkgsFor =
-        forAllSystems (system: import inputs.nixpkgs { inherit system; });
-
-    in {
-      homeConfigurations = {
-        user = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            (./. + "/profiles" + ("/" + systemSettings.profile) + "/home.nix") # load home.nix from selected PROFILE
-          ];
-          extraSpecialArgs = {
-            # pass config variables from above
-            inherit pkgs-stable;
-            inherit pkgs-emacs;
-            inherit pkgs-kdenlive;
-            inherit pkgs-nwg-dock-hyprland;
-            inherit systemSettings;
-            inherit userSettings;
-            inherit inputs;
-          };
-        };
-      };
-      nixosConfigurations = {
-        system = lib.nixosSystem {
-          system = systemSettings.system;
-          modules = [
-            (./. + "/profiles" + ("/" + systemSettings.profile) + "/configuration.nix")
-            ./system/bin/phoenix.nix
-          ]; # load configuration.nix from selected PROFILE
-          specialArgs = {
-            # pass config variables from above
-            inherit pkgs-stable;
-            inherit systemSettings;
-            inherit userSettings;
-            inherit inputs;
-          };
-        };
-      };
-      nixOnDroidConfigurations = {
-        inherit pkgs;
-        default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-          modules = [ ./profiles/nix-on-droid/configuration.nix ];
-        };
-        extraSpecialArgs = {
-          # pass config variables from above
-          inherit pkgs-stable;
-          inherit pkgs-emacs;
-          inherit systemSettings;
-          inherit userSettings;
-          inherit inputs;
-        };
-      };
-
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          default = self.packages.${system}.install;
-
-          install = pkgs.writeShellApplication {
-            name = "install";
-            runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
-            text = ''${./install.sh} "$@"'';
-          };
-        });
-
-      apps = forAllSystems (system: {
-        default = self.apps.${system}.install;
-
-        install = {
-          type = "app";
-          program = "${self.packages.${system}.install}/bin/install";
-        };
-      });
-    };
+  description = "NixOS Config";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
@@ -218,7 +20,8 @@
       inputs.home-manager.follows = "home-manager-unstable";
     };
 
-    hyprland = {
+    hyprland = 
+    {
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
       submodules = true;
@@ -239,11 +42,13 @@
     nix-straight.flake = false;
     nix-doom-emacs.inputs.nix-straight.follows = "nix-straight";
 
-    eaf = {
+    eaf = 
+    {
       url = "github:emacs-eaf/emacs-application-framework";
       flake = false;
     };
-    eaf-browser = {
+    eaf-browser = 
+    {
       url = "github:emacs-eaf/eaf-browser";
       flake = false;
     };
@@ -251,7 +56,8 @@
       url = "github:chrisbarrett/nursery";
       flake = false;
     };
-    org-yaap = {
+    org-yaap = 
+    {
       url = "gitlab:tygrdev/org-yaap";
       flake = false;
     };
@@ -297,4 +103,212 @@
       flake = false;
     };
   };
+
+  outputs = inputs@{ self, ... }:
+    let
+      # ---- SYSTEM SETTINGS ---- #
+      systemSettings = 
+      {
+        system = "x86_64-linux"; # system arch
+        hostname = "snowfire"; # hostname
+        profile = "personal"; # select a profile defined from my profiles directory
+        timezone = "America/Vancouver"; # select timezone
+        locale = "en_US.UTF-8"; # select locale
+        bootMode = "uefi"; # uefi or bios
+        bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+        grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+        gpuType = "intel"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
+      };
+
+      # ----- USER SETTINGS ----- #
+      userSettings = rec 
+      {
+        username = "fern "; # username
+        name = "fern"; # name/identifier
+        email = "fern@librephoenix.com"; # email (used for certain configurations)
+        dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
+        theme = "io"; # selcted theme from my themes directory (./themes/)
+        wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
+        # window manager type (hyprland or x11) translator
+        wmType = if (wm == "hyprland") then "wayland" else "x11";
+        browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
+        defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
+        term = "alacritty"; # Default terminal command;
+        font = "Intel One Mono"; # Selected font
+        fontPkg = pkgs.intel-one-mono; # Font package
+        editor = "emacsclient"; # Default editor;
+        # editor spawning translator
+        # generates a command that can be used to spawn editor inside a gui
+        # EDITOR and TERM session variables must be set in home.nix or other module
+        # I set the session variable SPAWNEDITOR to this in my home.nix for convenience
+        spawnEditor = if (editor == "emacsclient") then
+                        "emacsclient -c -a 'emacs'"
+                      else
+                        (if ((editor == "vim") ||
+                             (editor == "nvim") ||
+                             (editor == "nano")) then
+                               "exec " + term + " -e " + editor
+                         else
+                           editor);
+      };
+
+      # create patched nixpkgs
+      nixpkgs-patched =
+        (import inputs.nixpkgs { system = systemSettings.system; rocmSupport = (if systemSettings.gpu == "amd" then true else false); }).applyPatches 
+        {
+          name = "nixpkgs-patched";
+          src = inputs.nixpkgs;
+          patches = [ ./patches/emacs-no-version-check.patch ];
+        };
+
+      # configure pkgs
+      # use nixpkgs if running a server (homelab or worklab profile)
+      # otherwise use patched nixos-unstable nixpkgs
+      pkgs = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
+              then
+                pkgs-stable
+              else
+                (import nixpkgs-patched {
+                  system = systemSettings.system;
+                  config = {
+                    allowUnfree = true;
+                    allowUnfreePredicate = (_: true);
+                  };
+                  overlays = [ inputs.rust-overlay.overlays.default ];
+                }));
+
+      pkgs-stable = import inputs.nixpkgs-stable 
+      {
+        system = systemSettings.system;
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
+        };
+      };
+
+      pkgs-unstable = import inputs.nixpkgs-patched {
+        system = systemSettings.system;
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
+        };
+        overlays = [ inputs.rust-overlay.overlays.default ];
+      };
+
+      pkgs-emacs = import inputs.emacs-pin-nixpkgs {
+        system = systemSettings.system;
+      };
+
+      pkgs-kdenlive = import inputs.kdenlive-pin-nixpkgs {
+        system = systemSettings.system;
+      };
+
+      pkgs-nwg-dock-hyprland = import inputs.nwg-dock-hyprland-pin-nixpkgs {
+        system = systemSettings.system;
+      };
+
+      # configure lib
+      # use nixpkgs if running a server (homelab or worklab profile)
+      # otherwise use patched nixos-unstable nixpkgs
+      lib = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "worklab"))
+             then
+               inputs.nixpkgs-stable.lib
+             else
+               inputs.nixpkgs.lib);
+
+      # use home-manager-stable if running a server (homelab or worklab profile)
+      # otherwise use home-manager-unstable
+      home-manager = inputs.home-manager-stable;
+
+      # Systems that can run tests:
+      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+
+      # Function to generate a set based on supported systems:
+      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+
+      # Attribute set of nixpkgs for each system:
+      nixpkgsFor =
+        forAllSystems (system: import inputs.nixpkgs { inherit system; });
+
+    in 
+    {
+      homeConfigurations = 
+      {
+        user = home-manager.lib.homeManagerConfiguration 
+        {
+          inherit pkgs;
+          modules = [
+            (./. + "/profiles" + ("/" + systemSettings.profile) + "/home.nix") # load home.nix from selected PROFILE
+          ];
+          extraSpecialArgs = {
+            # pass config variables from above
+            inherit pkgs-stable;
+            inherit pkgs-emacs;
+            inherit pkgs-nwg-dock-hyprland;
+            inherit systemSettings;
+            inherit userSettings;
+            inherit inputs;
+          };
+        };
+      };
+      nixosConfigurations = 
+      {
+        system = lib.nixosSystem 
+        {
+          system = systemSettings.system;
+          modules = 
+          [
+            (./. + "/profiles" + ("/" + systemSettings.profile) + "/configuration.nix")
+            ./system/bin/phoenix.nix
+          ]; # load configuration.nix from selected PROFILE
+          specialArgs = 
+          {
+            # pass config variables from above
+            inherit pkgs-stable;
+            inherit systemSettings;
+            inherit userSettings;
+            inherit inputs;
+          };
+        };
+      };
+      nixOnDroidConfigurations = 
+      {
+        inherit pkgs;
+        default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+          modules = [ ./profiles/nix-on-droid/configuration.nix ];
+        };
+        extraSpecialArgs = {
+          # pass config variables from above
+          inherit pkgs-stable;
+          inherit pkgs-emacs;
+          inherit systemSettings;
+          inherit userSettings;
+          inherit inputs;
+        };
+      };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in 
+        {
+          default = self.packages.${system}.install;
+
+          install = pkgs.writeShellApplication {
+            name = "install";
+            runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
+            text = ''${./install.sh} "$@"'';
+          };
+        });
+
+      apps = forAllSystems (system: 
+      {
+        default = self.apps.${system}.install;
+
+        install = 
+        {
+          type = "app";
+          program = "${self.packages.${system}.install}/bin/install";
+        };
+      });
+    };
 }
--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -1,4 +1,5 @@
-{ inputs, pkgs, lib, ... }: let
+{ inputs, pkgs, lib, ... }: 
+let
   pkgs-hyprland = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in
 {
@@ -9,25 +10,30 @@ in
             ];
 
   # Security
-  security = {
+  security = 
+  {
     pam.services.login.enableGnomeKeyring = true;
   };
 
   services.gnome.gnome-keyring.enable = true;
 
-  programs = {
-    hyprland = {
+  programs = 
+  {
+    hyprland = 
+    {
       enable = true;
       package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-      xwayland = {
+      xwayland = 
+      {
         enable = true;
       };
       portalPackage = pkgs-hyprland.xdg-desktop-portal-hyprland;
     };
   };
-  environment = {
-    plasma5.excludePackages = [ pkgs.kdePackages.systemsettings ];
-    plasma6.excludePackages = [ pkgs.kdePackages.systemsettings ];
+  environment = 
+  {
+    plasma5.excludePackages = [ pkgs.kdePackages.systemsettings];
+    plasma6.excludePackages = [ pkgs.kdePackages.systemsettings];
   };
   services.xserver.excludePackages = [ pkgs.xterm ];
 }
